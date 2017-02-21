9d3ea4e553eaa2678de5ed7917e333338abb1d2a
Matthew Gerber <mmgerber@cableone.net>
2017-02-21 11:34:54 -0700

    Created base classes for teacher dashboard

---
+++ app/src/main/java/com/brainiacs/seandroidapp/TeacherDashboard/ButtonOnClickListener.java
@@ -0,0 +1,22 @@
+package com.brainiacs.seandroidapp.TeacherDashboard;
+
+import android.view.View;
+
+/**
+ * Created by Matthew on 2/21/17.
+ */
+
+public class ButtonOnClickListener implements View.OnClickListener {
+    private final int position;
+
+    public ButtonOnClickListener(int position)
+    {
+        this.position = position;
+    }
+
+    @Override
+    public void onClick(View v) {
+        //TODO
+        //When clicked do something(this.position)
+    }
+}
