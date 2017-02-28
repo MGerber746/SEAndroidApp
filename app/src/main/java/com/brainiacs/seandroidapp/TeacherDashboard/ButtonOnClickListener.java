c990da28d6081f85d85321be02036d88634e4133
Matthew Gerber <mmgerber@cableone.net>
2017-02-28 10:13:37 -0700

    Minor changes to allow creating students

--- app/src/main/java/com/brainiacs/seandroidapp/TeacherDashboard/ButtonOnClickListener.java
+++ app/src/main/java/com/brainiacs/seandroidapp/TeacherDashboard/ButtonOnClickListener.java
@@ -1,6 +1,8 @@
 package com.brainiacs.seandroidapp.TeacherDashboard;
 
+import android.content.Intent;
 import android.view.View;
+
 
 /**
  * Created by Matthew on 2/21/17.
@@ -10,14 +12,13 @@ import android.view.View;
 public class ButtonOnClickListener implements View.OnClickListener {
     private final int position;
 
-    public ButtonOnClickListener(int position)
-    {
+    public ButtonOnClickListener(int position) {
+
         this.position = position;
     }
 
     @Override
     public void onClick(View v) {
-        //TODO
-        //When clicked do something(this.position)
+        //Intent intent = new Intent(this, ClassHomeActivity.class);
     }
 }
--- app/src/main/java/com/brainiacs/seandroidapp/TeacherDashboard/ClassHomeActivity.java
+++ app/src/main/java/com/brainiacs/seandroidapp/TeacherDashboard/ClassHomeActivity.java
@@ -3,11 +3,13 @@ package com.brainiacs.seandroidapp.TeacherDashboard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.brainiacs.seandroidapp.R;
+
 public class ClassHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_class_home);
+        setContentView(R.layout.activity_class_home);
     }
 }
