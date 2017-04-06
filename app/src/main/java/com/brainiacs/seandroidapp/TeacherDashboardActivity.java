6a5ab2f031dab713695549b03ba6c305dbff44d8
Matthew Gerber <mmgerber@cableone.net>
2017-04-06 11:09:12 -0600

    Class buttons created by db

--- app/src/main/java/com/brainiacs/seandroidapp/TeacherDashboardActivity.java
+++ app/src/main/java/com/brainiacs/seandroidapp/TeacherDashboardActivity.java
@@ -1,4 +1,4 @@
-package com.brainiacs.seandroidapp.TeacherDashboard;
+package com.brainiacs.seandroidapp;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -11,18 +11,30 @@ import android.widget.TextView;
 import com.brainiacs.seandroidapp.R;
 import com.brainiacs.seandroidapp.LoginSelectionActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import utils.ButtonAdapter;
 import utils.DBTools;
+import utils.HttpURLConnectionHandler;
+import utils.JSONTool;
+import utils.StudentClassesURLConnectionHandler;
 
 /**
  * Created by Matthew on 2/21/17.
  * Sets up grid view with our buttons
  */
 public class TeacherDashboardActivity extends AppCompatActivity {
+    private static ArrayList<JSONObject> classes_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initializeData();
         setContentView(R.layout.activity_teacher_dashboard);
+        //TODO
         TextView view = (TextView) findViewById(R.id.username);
         view.setText("Teacher_Test");
 
@@ -51,6 +63,29 @@ public class TeacherDashboardActivity extends AppCompatActivity {
         finish();
     }
 
+    private void initializeData() {
+        JSONTool jsonTool = new JSONTool();
+        StudentClassesURLConnectionHandler handler = new StudentClassesURLConnectionHandler(
+                "teachers/get-classes", "Fetched classes", "Failed to fetch classes",
+                HttpURLConnectionHandler.Method.GET, null, this, null, jsonTool);
+        handler.execute((Void) null);
+        try {
+            // Sleep while we wait for the data
+            while (jsonTool.getJsonArray() == null) {
+                Thread.sleep(1000);
+            }
+        } catch(InterruptedException e) {}
 
+        classes_data = new ArrayList<>();
+        try {
+            for (int i = 0; i < jsonTool.getJsonArray().length(); ++i) {
+                classes_data.add(jsonTool.getJsonArray().getJSONObject(i));
+            }
+        } catch(JSONException e) {}
+    }
 
+    //Returns JSONClassData
+    public static ArrayList<JSONObject> getClassData(){
+        return classes_data;
+    }
 }
